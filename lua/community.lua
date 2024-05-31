daa50fa32033638667971b884ec8b823191294db
Yi Huang <yhuang72@apple.com>
2024-05-31 10:25:04 -0700

    feat: add mappings and user plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,17 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.workflow.bad-practices-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  {
+    "m4xshen/hardtime.nvim",
+    opts = {
+      disabled_keys = {
+        ["<Up>"] = {},
+        ["<Down>"] = {},
+      },
+    },
+  },
 }
