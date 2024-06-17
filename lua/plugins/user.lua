90a64f96f18399d5d14d203e27c4c71ded69d493
Yi Huang <yhuang72@apple.com>
2024-06-17 10:50:28 -0700

    fix: disable notify

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -41,6 +39,8 @@ return {
 
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
+
+  { "rcarriga/nvim-notify", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
